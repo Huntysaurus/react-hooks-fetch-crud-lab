65a84c621ade2f50267f839f2663fad95edfe4a5
Jacob <yawjacob@yahoo.com>
2022-01-23 19:21:26 -0600

    commit tree 2e53bdfa67751a2b4d147f046c2f4d9fb51fe1e2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questionsData, setQuestionsData] = useState([])
 
+  useEffect(()=> {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(data => setQuestionsData(data))
+  }, [])
+
+
+ function handleAddQuestion(newQuestion) {
+   console.log(newQuestion)
+   setQuestionsData([...questionsData, newQuestion])
+ }
+
+ function handleDeleteQuestion(deletedQuestion) {
+  const UpdatedQuestions = questionsData.filter(question => question.id !== deletedQuestion.id)
+  setQuestionsData(UpdatedQuestions)
+}
+
+function handleUpdatedQuestion(updatedQuestion) {
+  console.log(updatedQuestion)
+  const updatedQuestions = questionsData.map((question) => {
+    if (question.correctIndex !== updatedQuestion.correctIndex) {
+      return updatedQuestion
+    } else {
+      return question
+    }
+  })
+  setQuestionsData(updatedQuestions)
+}
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? 
+        <QuestionForm onAddQuestion={handleAddQuestion}/>
+       : 
+        <QuestionList onRemoveQuestion={handleDeleteQuestion} onChangedQuestion={handleUpdatedQuestion} questionsData={questionsData}/>}
     </main>
   );
 }

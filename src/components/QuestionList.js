229d03fc250a124d45c5a5d70ec5950e0d438e9d
Jacob Yaw <yawjacob@yahoo.com>
2022-02-23 17:33:43 -0600

    testing to see if this works

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList({ onChangedQuestion, questionsData, onRemoveQuestion }) {
+
+  const individualQuestion = questionsData.map(question => <QuestionItem key={question.prompt} question={question} onUpdateQuestion={onChangedQuestion} onDeleteQuestion={onRemoveQuestion}/>)
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{individualQuestion}</ul>
     </section>
   );
 }
